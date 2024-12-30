caf70ba6dc325001c6e53bb6eea07f817b56530a
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-12-30 21:30:35 +0100

    add envoriment

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,41 @@
+export const environment = {
+    apiUrl: 'http://localhost:8080',
+  
+    userController: {
+      user: '/api/user',
+      actionUser: '/api/user/',
+      listUser: '/api/user/list'
+    },
+  
+    plantesController: {
+      plantes: '/api/plantes',
+      actionPlantes: '/api/plantes/',
+      commentaire: '/api/plantes/commentaire/',
+      listPlantes: '/api/plantes/list'
+    },
+  
+    maladiesController: {
+      maladie: '/api/maladies',
+      actionMaladie: '/api/maladies/',
+      listMaladies: '/api/maladies/list'
+    },
+  
+    compteController: {
+      Profile: '/api/compte',
+    },
+  
+    articleController: {
+      article: '/api/articles',
+      actionArticle: '/api/articles/',
+      commentaire: '/api/articles/commentaire/',
+      listArticles: '/api/articles/list'
+    },
+  
+    authController: {
+      register: '/auth/register',
+      refreshToken: '/auth/refresh',
+      login: '/auth/login',
+      logout: '/api/auth/logout'
+    }
+  };
+  
